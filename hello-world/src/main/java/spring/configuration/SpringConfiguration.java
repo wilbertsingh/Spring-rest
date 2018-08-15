7b726c46799d3a5791a0ae9d8d05e3164ae7c92a
Mathanlal sait <mathanlalsait@gmail.com>
2018-08-15 16:40:01 +0530

    hello world

---
+++ hello-world/src/main/java/spring/configuration/SpringConfiguration.java
@@ -0,0 +1,13 @@
+package spring.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "spring.controller")
+public class SpringConfiguration {
+
+}
